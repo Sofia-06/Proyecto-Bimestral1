9b584915ee59de54623b8012ce181d8ab1a3a978
Sofia Abigail Sabán Cárcamo <201800090@colsanjose.edu.gt>
2020-07-09 15:26:42 -0600

    Primera Parte

---
+++ Primera Parte, proyecto/Primera Parte, proyecto/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Primera_Parte__proyecto
+{
+    class Program
+    {
+        static SerialPort Arduino = new SerialPort("COM27", 9600);
+        static void Main(string[] args)
+        {
+            Arduino = Open();
+
+            Arduino.Parity = Parity.None;
+            Arduino.StopBits = StopBits.One;
+            Arduino.DataBits = 8;
+            Arduino.Handshake = Handshake.None;
+            Arduino.RtsEnable = true;
+
+            // Mi grupo: Keyli Solis, Fatima Ventura, y mi persona: (Sofia Sabán)
+
+            if (Arduino.IsOpen)
+            {
+                Console.WriteLine("Com abierto");
+                byte[] data = Encoding.ASCII.GetBytes("z");
+                Arduino.Write(data, 0, data.Length);
+
+                Console.WriteLine("Com abierto");
+                byte[] data1 = Encoding.ASCII.GetBytes("p");
+                Arduino.Write(data1, 0, data1.Length);
+
+                Console.WriteLine("Com abierto");
+                byte[] data2 = Encoding.ASCII.GetBytes("k");
+                Arduino.Write(data2, 0, data2.Length);
+                Arduino.Write("l");
+            }
+            else
+            {
+                Console.WriteLine("Com cerrado");
+            }
+
+            Console.ReadKey();
+            Arduino.Close();
+        }
+    }
+}
